c8bfd3f7a408d592c2f200337d4e0d981dc9c6fc
tfwright <tfwright@gmail.com>
2010-12-11 12:22:19 -0600

    implements basic click adjustment, drag not functioning

--- adjustable-pie-chart.js
+++ adjustable-pie-chart.js
@@ -8,25 +8,74 @@ Raphael.fn.piechart = function (offsetX, offsetY, radius, values) {
 			total += values[i];
 		}
 		
-		function sliceDef(value, startAngle, endAngle) {
+		function sliceDef(startAngle, endAngle) {
+				if(Math.abs(endAngle) > Math.abs(startAngle)){
+					var greaterThan180 = (Math.abs(endAngle - startAngle) > 180);
+				} else {
+					var greaterThan180 = ((360 - Math.abs(startAngle) + Math.abs(endAngle)) > 180);
+				}
         var rad = Math.PI/180,
-            x1 = offsetX + radius * Math.cos(-startAngle * rad),
-            x2 = offsetX + radius * Math.cos(-endAngle * rad),
-            y1 = offsetY + radius * Math.sin(-startAngle * rad),
-            y2 = offsetY + radius * Math.sin(-endAngle * rad),
-            pathDef = ["M", offsetX, offsetY, "L", x1, y1, "A", radius, radius, 0, +(Math.abs(endAngle - startAngle) > 180), 1, x2, y2, "z"];
+            x1 = offsetX + radius * Math.cos(Math.abs(startAngle) * rad),
+            x2 = offsetX + radius * Math.cos(Math.abs(endAngle) * rad),
+            y1 = offsetY + radius * Math.sin(Math.abs(startAngle) * rad),
+            y2 = offsetY + radius * Math.sin(Math.abs(endAngle) * rad),
+            pathDef = ["M", offsetX, offsetY, "L", x1, y1, "A", radius, radius, 0, +greaterThan180, 1, x2, y2, "z"];
         return pathDef;
     }
-		
-    paper.circle(offsetX, offsetY, radius).attr({"stroke":"#000", "stroke-width":2});
 
 		for(var i = 0; i < values.length; i++){
 			var degrees = (360 * values[i]/total)
 			var endAngle = startAngle - degrees;
-			var slice = this.path(sliceDef(values[i], startAngle, endAngle)).attr({stroke: "#000"});
+			var slice = this.path(sliceDef(startAngle, endAngle)).attr({stroke: "#000", "stroke-width": 2, fill: "#fff"});
+			slice.startAngle = startAngle;
+			slice.endAngle = endAngle;
 			chart.push(slice)
 			startAngle += (endAngle - startAngle);
 		}
+		
+		chart.drag(function(){
+			},
+			function(dx, dy, event){
+				this.toFront();
+				var startAngle = -Raphael.angle(event.pageX-paper.canvas.offsetLeft, event.pageY-paper.canvas.offsetTop, offsetX, offsetY);
+				this.attr("path", sliceDef(startAngle, this.endAngle));
+				this.startAngle = startAngle;
+				var prevSlice = chart[this.id-1];
+				if(prevSlice==undefined){
+					prevSlice = chart[chart.length-1]
+				}
+				prevSlice.attr("path", sliceDef(prevSlice.startAngle, startAngle));
+				prevSlice.endAngle = startAngle;
+			},
+			function(event){
+		}); 
+		
+		//chart.mousedown(function(event){
+		//	alert("chart!")
+		//	var clickAngle = Raphael.angle(offsetX, offsetY, event.pageX-paper.canvas.offsetLeft, event.pageY-paper.canvas.offsetTop)
+		//	for(var i = 0; i < chart.length; i++){
+		//		var slicePoint = chart[0].getPointAtLength(100);
+		//		var thisAngle = Raphael.angle(offsetX, offsetY, slicePoint.x, slicePoint.y);
+		//		if(thisAngle==clickAngle){
+		//			var slice = chart[i];
+		//			alert("good click!")
+		//			break;
+		//		}
+		//	}
+		//	if(slice==undefined){
+		//		alert("misclick")
+		//		return false;
+		//	}
+		//	chart.mousemove(function(event){
+		//		var startAngle = Raphael.angle(offsetX, offsetY, event.pageX-paper.canvas.offsetLeft, event.pageY-paper.canvas.offsetTop)
+		//		var endPoint = slice.getPointAtLength(slice.getTotalLength());
+		//		var endAngle = Raphael.angle(offsetX, offsetY, endPoint.x, endPoint.y);
+		//		slice.attr("path", sliceDef(startAngle, endAngle));
+		//	});
+		//});
+		//chart.mouseup(function(event){
+		//	slice.unmousemove();
+		//});
 		
 		return chart
 		

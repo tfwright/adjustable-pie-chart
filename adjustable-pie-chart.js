c00c37c3c61e3bcfa411cbda96748e030e07b632
tfwright <tfwright@gmail.com>
2010-12-11 14:25:45 -0600

    implemented basic resizing limitations

--- adjustable-pie-chart.js
+++ adjustable-pie-chart.js
@@ -8,18 +8,21 @@ Raphael.fn.piechart = function (offsetX, offsetY, radius, values) {
 			total += values[i];
 		}
 		
+		function degreesBetween(startAngle, endAngle){
+			if(-endAngle < -startAngle){
+				return 360 - -startAngle + -endAngle
+			} else {
+				return -endAngle - -startAngle
+			}
+		}
+		
 		function sliceDef(startAngle, endAngle) {
-				if(Math.abs(endAngle) > Math.abs(startAngle)){
-					var greaterThan180 = (Math.abs(endAngle - startAngle) > 180);
-				} else {
-					var greaterThan180 = ((360 - Math.abs(startAngle) + Math.abs(endAngle)) > 180);
-				}
         var rad = Math.PI/180,
             x1 = offsetX + radius * Math.cos(Math.abs(startAngle) * rad),
             x2 = offsetX + radius * Math.cos(Math.abs(endAngle) * rad),
             y1 = offsetY + radius * Math.sin(Math.abs(startAngle) * rad),
             y2 = offsetY + radius * Math.sin(Math.abs(endAngle) * rad),
-            pathDef = ["M", offsetX, offsetY, "L", x1, y1, "A", radius, radius, 0, +greaterThan180, 1, x2, y2, "z"];
+            pathDef = ["M", offsetX, offsetY, "L", x1, y1, "A", radius, radius, 0, +(degreesBetween(startAngle, endAngle) > 180), 1, x2, y2, "z"];
         return pathDef;
     }
 
@@ -35,24 +38,39 @@ Raphael.fn.piechart = function (offsetX, offsetY, radius, values) {
 		
 		chart.drag(
 			function(dx, dy){
-				this.toFront();
 				var startAngle = -Raphael.angle(this.startX+dx, this.startY+dy, offsetX, offsetY);
-				this.attr("path", sliceDef(startAngle, this.endAngle));
-				this.startAngle = startAngle;
 				var prevSlice = chart[this.id-1];
 				if(prevSlice==undefined){
 					prevSlice = chart[chart.length-1]
 				}
+				if(degreesBetween(startAngle, this.endAngle)<=5 || degreesBetween(prevSlice.startAngle, startAngle)<=5){
+					fireEvent(this.node, "mouseup");
+				}
+				this.attr("path", sliceDef(startAngle, this.endAngle));
+				this.startAngle = startAngle;
 				prevSlice.attr("path", sliceDef(prevSlice.startAngle, startAngle));
 				prevSlice.endAngle = startAngle;
 			},
 			function(x, y){
+					this.toFront();
 					this.startX = x;
 					this.startY = y;
-				},
-			function(){
 		}); 
 		
 		return chart
 		
 };
+
+function fireEvent(element,event){
+    if (document.createEventObject){
+    // dispatch for IE
+    var evt = document.createEventObject();
+    return element.fireEvent('on'+event,evt)
+    }
+    else{
+    // dispatch for firefox + others
+    var evt = document.createEvent("HTMLEvents");
+    evt.initEvent(event, true, true ); // event type,bubbling,cancelable
+    return !element.dispatchEvent(evt);
+    }
+}

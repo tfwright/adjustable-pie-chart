a9cedf58dad7bbd4e3d71cdf33a7b7b1572f9040
tfwright <tfwright@gmail.com>
2010-12-10 18:35:14 -0600

    returns chart as set of drawn slices

--- adjustable-pie-chart.js
+++ adjustable-pie-chart.js
@@ -1,10 +1,14 @@
 Raphael.fn.piechart = function (offsetX, offsetY, radius, values) {
+		
+		var paper = this;
+		var chart = this.set();
+		var startAngle = 0;
 		var total = 0;
 		for(var i = 0; i < values.length; i++){
 			total += values[i];
 		}
-    this.circle(offsetX, offsetY, radius).attr({"stroke":"#000", "stroke-width":2});
-    function slice(value, startAngle, endAngle) {
+		
+		function sliceDef(value, startAngle, endAngle) {
         var rad = Math.PI/180,
             x1 = offsetX + radius * Math.cos(-startAngle * rad),
             x2 = offsetX + radius * Math.cos(-endAngle * rad),
@@ -13,11 +17,17 @@ Raphael.fn.piechart = function (offsetX, offsetY, radius, values) {
             pathDef = ["M", offsetX, offsetY, "L", x1, y1, "A", radius, radius, 0, +(Math.abs(endAngle - startAngle) > 180), 1, x2, y2, "z"];
         return pathDef;
     }
-		var startAngle = 0;
+		
+    paper.circle(offsetX, offsetY, radius).attr({"stroke":"#000", "stroke-width":2});
+
 		for(var i = 0; i < values.length; i++){
 			var degrees = (360 * values[i]/total)
 			var endAngle = startAngle - degrees;
-			this.path(slice(values[i], startAngle, endAngle)).attr({stroke: "#000"});
+			var slice = this.path(sliceDef(values[i], startAngle, endAngle)).attr({stroke: "#000"});
+			chart.push(slice)
 			startAngle += (endAngle - startAngle);
 		}
+		
+		return chart
+		
 };
